6124ff23028092173a3b41185611c3baf7878391
gmendes0 <gmendes230@gmail.com>
2019-02-02 17:13:50 -0200

    Start Test

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Login</title>
+        <link rel="stylesheet" type="text/css" href="main.css"/>
+    </head>
+
+    <body>
+
+        <form method="post" class="box">
+
+            <h1>Login</h1>
+            
+            <input type="text" name="user" placeholder="Username"/>
+            <input type="password" name="senha" placeholder="Senha"/>
+            <input type="submit" value="login"/>
+
+            <a href="#">Cadastre-se</a>
+
+        </form>
+
+    </body>
+</html>
